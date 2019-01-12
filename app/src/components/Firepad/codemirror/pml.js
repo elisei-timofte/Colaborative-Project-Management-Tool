d6d12f5f5ce68e67a156c942a92df37b26ff252b
Elisei Timofte <timofte@unfuddle.com>
2019-01-12 18:52:37 +0200

    add role to the alias token

--- app/src/components/Firepad/codemirror/pml.js
+++ app/src/components/Firepad/codemirror/pml.js
@@ -4,7 +4,7 @@ import './pml.scss'
 global.CodeMirror.defineSimpleMode("pml", {
   // The start state contains the rules that are intially used
   start: [
-    {regex: /@[a-z]+/, token: "pml-alias"},
+    {regex: /@[a-z]+(:[a-z]+)?/, token: "pml-alias"},
     {regex: /&[0-9\-]+/, token: "pml-date"},
   ],
 });
