420c91d86594d16259e742bf476e44d8baf9bd5e
Elisei Timofte <timofte@unfuddle.com>
2019-01-14 14:27:08 +0200

    moved the Firepad component under a private route

--- app/src/components/Home/Home.js
+++ app/src/components/Home/Home.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 import { withAuthorization } from '../Session';
 
+import Firepad from '../Firepad';
+import './home.scss';
+
 const HomePage = () => (
-  <div>
+  <div className='home-page'>
     <h1>Home Page</h1>
     <p>The Home Page is accessible by every signed in user.</p>
+
+    <Firepad />
   </div>
 );
 
--- app/src/components/Landing/Landing.js
+++ app/src/components/Landing/Landing.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-import Firepad from '../Firepad';
 import './landing.scss';
 
 const Landing = () => (
   <div className='landing-page'>
     Landing Page
-    <Firepad />
   </div>
 );
 
